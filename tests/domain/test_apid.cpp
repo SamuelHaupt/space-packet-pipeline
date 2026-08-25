8a314465e355e83a5de0d05cf0fd97a7ce565c88
Samuel Haupt <sg.haupt@icloud.com>
2026-08-24 22:18:38 -0600

    jira-303 test: set max range; test green #time 5m

--- tests/domain/test_apid.cpp
+++ tests/domain/test_apid.cpp
@@ -5,3 +5,8 @@ TEST_CASE("Apid stores a valid value", "[apid]") {
     Apid apid{100};
     REQUIRE(apid.value() == 100);
 }
+
+TEST_CASE("Apid accepts the maximum valid value", "[apid]") {
+    Apid apid{0x7FF};
+    REQUIRE(apid.value() == 0x7FF);
+}
